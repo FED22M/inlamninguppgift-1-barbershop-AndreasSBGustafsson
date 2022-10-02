536ad4428295fde2dfd7c698b0c834a02d1a32c6
Andreas Gustafsson <andreas.gustafsson9206@gmail.com>
2022-10-02 16:39:25 +0200

    JS menu phone view

---
+++ src/asset/js/nav-script.js
@@ -0,0 +1,8 @@
+const menuBtn = document.querySelector('#hamburger');
+const menu = document.querySelector('.menu');
+console.log(menuBtn)
+menuBtn.addEventListener('click', () => {
+    console.log('hej')
+menu.classList.toggle('menu-open');
+
+});
